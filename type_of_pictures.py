1f1da45cd3a89e0c8a93e77cffa38ee129c2634a
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2024-08-27 20:20:23 +0300

    Add update file

--- type_of_pictures.py
+++ type_of_pictures.py
@@ -4,39 +4,26 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
-import os
+from heic_finder import *
+from typing import List
 
 
-edge_driver_path = 'D:/edge_driver/msedgedriver.exe'
-service = EdgeService(executable_path=edge_driver_path)
-driver = webdriver.Edge(service=service)
+def used_web_for_changing_the_format(all_heic_pictures=List):
+    edge_driver_path = 'D:/edge_driver/msedgedriver.exe'
+    service = EdgeService(executable_path=edge_driver_path)
+    driver = webdriver.Edge(service=service)
 
 
-try:
-    driver.get("https://heic.online/")
-    wait = WebDriverWait(driver, 10)
-    consent_button = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[3]/div[2]/div[1]/div[2]/div[2]/button[1]')))
-    consent_button.click()
-    upload_element = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="zbasic"]/button')))
-    time.sleep(60)
-except:
-    print('Not working! Please check the XPATH or edge driver path! Be aware that you have install the webdriver before starting the script!')
+    try:
+        driver.get("https://heic.online/")
+        wait = WebDriverWait(driver, 10)
+        consent_button = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[3]/div[2]/div[1]/div[2]/div[2]/button[1]')))
+        consent_button.click()
+        upload_element = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="zbasic"]/button')))
+        time.sleep(60)
+    except:
+        print('Not working! Please check the XPATH or edge driver path! Be aware that you have install the webdriver before starting the script!')
 
 
-def find_all_heic_pictures(needed_path):
-    """Find all heic pictures in the path. The path is set to the main folder directory, it can be changed manually"""
-    heic_pictures = []
-    for file in os.listdir(needed_path):
-        if file.lower().endswith('.heic'):
-            heic_pictures.append(f'{needed_path}\{file}')
-            heic_file = f'{needed_path}\{file}'
-            jpeg_file = f'image.JPEG'
-
-            command = f"magick convert {heic_file} {jpeg_file}"
-
-    return heic_pictures
-
-
-path = os.getcwd()
-all_heic_pictures = find_all_heic_pictures(path)
-print(all_heic_pictures[0])
+heic_picture_list = find_all_heic_pictures()
+used_web_for_changing_the_format()
