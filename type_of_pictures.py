d5094159dc5278dd8e6833d2329217cbe44548b4
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2024-08-30 16:47:39 +0300

    Update file for selenium app

--- type_of_pictures.py
+++ type_of_pictures.py
@@ -8,44 +8,48 @@ from heic_finder import *
 from typing import List
 
 
-def used_web_for_changing_the_format(all_heic_pictures=List):
-    edge_driver_path = 'D:/edge_driver/msedgedriver.exe'
-    service = EdgeService(executable_path=edge_driver_path)
-    driver = webdriver.Edge(service=service)
+class PictureConvertor:
 
+    def __init__(self, heic_pictures=List):
+        self.heic_pictures = heic_pictures
 
-    try:
-        driver.get("https://heic.online/")
-        wait = WebDriverWait(driver, 10)
+    def used_web_for_changing_the_format(self):
+        edge_driver_path = 'D:/edge_driver/msedgedriver.exe'
+        service = EdgeService(executable_path=edge_driver_path)
+        driver = webdriver.Edge(service=service)
 
-        consent_button = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[3]/div[2]/div[1]/div[2]/div[2]/button[1]')))
-        consent_button.click()
+        try:
+            driver.get("https://heic.online/")
+            wait = WebDriverWait(driver, 10)
 
-        time.sleep(2)
+            consent_button = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[3]/div[2]/div[1]/div[2]/div[2]/button[1]')))
+            consent_button.click()
 
-        file_input = wait.until(EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div[1]/div/div/main/article/div/div[1]/div/div/div/form/input[2]')))
+            time.sleep(2)
 
-        for pic_path in heic_picture_list:
+            for pic_path in self.heic_pictures:
+                file_input = wait.until(EC.presence_of_element_located(
+                    (By.XPATH, '/html/body/div[1]/div[1]/div/div/main/article/div/div[1]/div/div/div/form/input[2]')))
+                file_input.send_keys(pic_path)
 
-            file_input.send_keys(pic_path)
+            time.sleep(5)
 
-        time.sleep(5)
+            check_box_consent = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div[1]/div/div/main/article/div/div[4]/input')))
+            check_box_consent.click()
 
-        check_box_consent = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div[1]/div/div/main/article/div/div[4]/input')))
-        check_box_consent.click()
+            time.sleep(2)
+            convert_button = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div[1]/div/div/main/article/div/p[1]/button')))
+            convert_button.click()
 
-        time.sleep(2)
-        convert_button = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div[1]/div/div/main/article/div/p[1]/button')))
-        convert_button.click()
+            time.sleep(2)
+            download_button = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div[1]/div/div/main/article/div/div[2]/div/button[1]')))
+            download_button.click()
 
-        time.sleep(2)
-        download_button = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div[1]/div/div/main/article/div/div[2]/div/button[1]')))
-        download_button.click()
-
-        time.sleep(15)
-    except:
-        print('Not working! Please check the XPATH or edge driver path! Be aware that you have install the webdriver before starting the script!')
+            time.sleep(15)
+        except:
+            print('Not working! Please check the XPATH or edge driver path! Be aware that you have install the webdriver before starting the script!')
 
 
 heic_picture_list = find_all_heic_pictures()
-used_web_for_changing_the_format(heic_picture_list)
+pictures_convertor = PictureConvertor(heic_picture_list)
+pictures_convertor.used_web_for_changing_the_format()
