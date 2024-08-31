2de9cf151cda694eb3bbd99ce4f6cf06187c712b
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2024-08-31 16:24:24 +0300

    Update file

--- type_of_pictures.py
+++ type_of_pictures.py
@@ -4,7 +4,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
-from heic_finder import *
 from typing import List
 
 
